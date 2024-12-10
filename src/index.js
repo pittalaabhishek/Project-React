39df4b1a096a82c4dca853fbf3e2a15accd83379
VenkatAbhi <pittalaabhishek999@gmali.com>
2024-12-10 12:35:11 +0530

    Added Rendering list files in same folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Apprenderlists from './Apprenderlists';
+import Apprenderlists from './ProductLists/Apprenderlists';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
